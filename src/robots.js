d56c1dd1bbbaec5b2dba9ef8c4c2f9659b400219
Isaac Osei Anane <61693488+callmehumble@users.noreply.github.com>
2021-02-15 16:01:09 +0100

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,114 @@
+export const robots = [
+    { 
+        id: 1,
+        name: 'Mimi Yang',
+        username: 'Mimi',
+        email: 'mimi@gmail.com'
+    },
+
+    { 
+        id: 2,
+        name: 'Yao Wiyao',
+        username: 'Yao',
+        email: 'yao@gmail.com'
+    },
+
+    { 
+        id: 3,
+        name:  'Flep Kait',
+        username:  'Flep',
+        email: 'flep@gmail.com'
+    },
+
+    { 
+        id: 4,
+        name: 'Niman Saed',
+        username: 'Niman',
+        email: 'niman@gmail.com'
+    },
+
+    { 
+        id: 5,
+        name: 'Zidat Kot',
+        username: 'Zidat',
+        email: 'zidat@gmail.com'
+    },
+
+    { 
+        id: 6,
+        name: 'Qwell It',
+        username: 'Qwell',
+        email: 'qwell@gmail.com'
+    },
+
+    { 
+        id: 7,
+        name: 'Just Ignore',
+        username: 'Just',
+        email: 'just@gmail.com'
+    },
+
+    { 
+        id: 8,
+        name: 'Iwill win',
+        username: 'Iwill',
+        email: 'Iwill@gmail.com'
+    },
+
+    { 
+        id: 9,
+        name: 'Be Great',
+        username: 'Be',
+        email: 'begreat@gmail.com'
+    },
+
+    { 
+        id: 10,
+        name: 'Iam Observing',
+        username: 'Observe',
+        email: 'observe@gmail.com'
+    },
+
+    { 
+        id: 11,
+        name: 'Mia Ann',
+        username: 'Mia',
+        email: 'miaann@gmail.com'
+    },
+
+    { 
+        id: 12,
+        name: 'Donot Sin',
+        username: 'Donot',
+        email: 'donotsin@gmail.com'
+    },
+
+    { 
+        id: 13,
+        name: 'Prosper Senyo',
+        username: 'Prosper',
+        email: 'psenyo@gmail.com'
+    },
+
+    { 
+        id: 14,
+        name: 'Oscar Mingah',
+        username: 'Oscar',
+        email: 'omingah@gmail.com'
+    },
+
+    { 
+        id: 15,
+        name: 'Edward Mensah',
+        username: 'Eddy',
+        email: 'emensah@gmail.com'
+    },
+
+    { 
+        id: 16,
+        name: 'Mister Humble',
+        username: 'Humble',
+        email: 'mhumble@gmail.com'
+    }
+
+]
